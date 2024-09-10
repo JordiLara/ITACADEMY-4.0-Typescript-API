717b344aa0009433007e8a599ebfd672e07414a0
JordiLara <jordilarareyes@gmail.com>
2024-09-10 21:47:53 +0200

    add API urls, and create .js from the TS file, so can link with HTML correctly.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,2 +1,17 @@
 
+const apiUrl: string = "https://icanhazdadjoke.com/";
 
+const options = {
+    method: "GET",
+    headers: {
+        "Accept": "application/json", 
+    },
+};
+
+function randomJoke() {
+    fetch(apiUrl, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+    });
+}
