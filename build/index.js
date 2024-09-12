baeefb14d1f3027ad2efd4092b8b42a1f1a2bfe8
JordiLara <jordilarareyes@gmail.com>
2024-09-12 17:40:25 +0200

    add functions and HTML elements to rate jokes and generate an Array with the scores

--- build/index.js
+++ build/index.js
@@ -33,8 +33,59 @@ function getRandomJoke() {
 document.addEventListener("DOMContentLoaded", () => {
     getRandomJoke();
 });
+function nextJoke() {
+    getData();
+    getRandomJoke();
+}
+const reportJokes = [];
+// gets the data from the votations 
+function getData() {
+    const options = document.getElementsByName('option');
+    let choosenOption = null;
+    let score = 0;
+    options.forEach(option => {
+        if (option.checked) {
+            choosenOption = option.value;
+        }
+    });
+    if (choosenOption !== null) {
+        if (choosenOption === 'option1') {
+            score = 1;
+        }
+        else if (choosenOption === 'option2') {
+            score = 2;
+        }
+        else if (choosenOption === 'option3') {
+            score = 3;
+        }
+        const dateISO = createDateISO();
+        const randomJoke = document.getElementById('randomJokes');
+        const jokeText = randomJoke.innerText;
+        const jokeObject = {
+            joke: jokeText,
+            score: score,
+            date: dateISO,
+        };
+        reportJokes.push(jokeObject);
+        console.log(reportJokes);
+    }
+    else {
+        console.log('no options choosen');
+    }
+    uncheck();
+}
+function uncheck() {
+    const options = document.getElementsByName('option');
+    options.forEach(option => {
+        option.checked = false;
+    });
+}
+function createDateISO() {
+    const actualDate = new Date();
+    const dateISO = actualDate.toISOString();
+    return dateISO;
+}
 /* To-DO
-panel de votación
 añadir api del tiempo
 estilos =(
 
--- src/index.ts
+++ src/index.ts
@@ -44,11 +44,86 @@ function getRandomJoke(): Promise<string> {
 // throws a joke when the web page loads.
 
 document.addEventListener("DOMContentLoaded", () => {
+
     getRandomJoke();
 });
 
+function nextJoke() {
+
+    getData();
+    getRandomJoke();
+}
+
+// which type of array format we want to generate
+
+type JOKE = {
+    joke: string;
+    score: number;
+    date: string; 
+};
+
+const reportJokes: JOKE[] = [];
+
+// gets the data from the votations 
+
+function getData() {
+
+    const options = document.getElementsByName('option') as NodeListOf<HTMLInputElement>;
+    let choosenOption: string | null = null;
+    let score: number = 0;
+
+    options.forEach(option => {
+        if (option.checked) {
+            choosenOption = option.value;
+        }
+    });
+
+    if (choosenOption !== null) {
+       
+        if (choosenOption === 'option1') {
+            score = 1; 
+        } else if (choosenOption === 'option2') {
+            score = 2 
+        } else if (choosenOption === 'option3') {
+            score = 3
+        }
+        
+        const dateISO = createDateISO();
+        const randomJoke: HTMLElement | null = document.getElementById('randomJokes')!;
+        const jokeText = randomJoke.innerText;
+
+        const jokeObject: JOKE = {
+            joke: jokeText,
+            score: score,
+            date: dateISO,
+        };
+
+        reportJokes.push(jokeObject);
+        console.log(reportJokes);
+
+    } else {
+        console.log('no options choosen');
+    }
+
+    uncheck();
+}
+
+function uncheck() {
+
+    const options = document.getElementsByName('option') as NodeListOf<HTMLInputElement>;
+    options.forEach(option => {
+        option.checked = false;
+    })
+}
+
+function createDateISO() {
+
+    const actualDate = new Date();
+    const dateISO = actualDate.toISOString();
+    return dateISO;
+}
+
 /* To-DO 
-panel de votación
 añadir api del tiempo
 estilos =(
 
